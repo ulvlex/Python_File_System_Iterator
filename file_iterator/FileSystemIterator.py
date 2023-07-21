a27d2ca0b19c43449cbec42a34b5c18b1d52cbd9
uvlasova <ulyanaspr@mail.ru>
2023-07-21 21:40:31 +0300

    corrections

--- file_iterator/FileSystemIterator.py
+++ file_iterator/FileSystemIterator.py
@@ -6,6 +6,13 @@ class FileSystemIterator:
         self.only_files = only_files
         self.only_dirs = only_dirs
         self.pattern = pattern
+
+        if not os.path.exists(self.root):
+            raise FileNotFoundError("The root path does not exist.")
+
+        if self.only_files and self.only_dirs:
+            raise ValueError("Both 'only_files' and 'only_dirs' cannot be True")
+
         self.generator = self.generate()
 
     def __iter__(self):
@@ -16,14 +23,14 @@ class FileSystemIterator:
 
     def generate(self):
         for path, dirs, files in os.walk(self.root):
-            if self.only_files or (self.only_files is False and self.only_dirs is False):
-                yield from (self.check_pattern(file) for file in files)
+            if self.only_files is False:
+                yield from (os.path.join(path, directory).replace(os.sep, '/') for directory in dirs if self.check_pattern(directory))
 
-            if self.only_dirs or (self.only_files is False and self.only_dirs is False):
-                yield from (self.check_pattern(directory) for directory in dirs)
+            if self.only_dirs is False:
+                yield from (os.path.join(path, file).replace(os.sep, '/') for file in files if self.check_pattern(file))
 
     def check_pattern(self, object):
         if self.pattern is None or self.pattern in object:
-            return os.path.join(self.root, object)
+            return True
         else:
-            return None
+            return False
